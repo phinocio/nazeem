536bf7f3f02b08025eabb50ca1b1f38b2a6b39c3
Phinocio <github@phinocio.com>
2019-11-12 12:50:19 -0700

    rework index to 2 files

---
+++ src/App.ts
@@ -0,0 +1,4 @@
+import Bot from './Bot';
+
+const bot = new Bot();
+bot.login();
---
+++ src/Bot.ts
@@ -0,0 +1,36 @@
+import Discord from 'discord.js';
+import { auth } from '../config.json';
+import MessageHandler from './Handlers/MessageHandler';
+
+// bot
+class Bot {
+    private TOKEN = '';
+    private client: Discord.Client;
+    private msgHandler: MessageHandler | undefined;
+
+    public constructor() {
+        this.TOKEN = auth.token;
+
+        this.client = new Discord.Client();
+
+        this.client.on('ready', () => this.ready());
+    }
+
+    public async login(): Promise<void> {
+        try {
+            await this.client.login(this.TOKEN);
+        } catch (err) {
+            console.error(`Error: ${err.message}`);
+        }
+    }
+
+    private ready(): void {
+        console.log(`Logged in as ${this.client.user.tag}!`);
+        this.client.user.setActivity('!h or !help');
+
+        this.msgHandler = new MessageHandler(this.client);
+        this.msgHandler.handle();
+    }
+}
+
+export default Bot;
