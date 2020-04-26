f8efbb13851b99c3c4fdcb99fd4f118ad3a26aae
Phinocio <github@phinocio.com>
2020-04-26 15:28:05 -0600

    remake faction command

--- src/Commands/CommandList.ts
+++ src/Commands/CommandList.ts
@@ -1,9 +1,14 @@
 import Command from '../Interfaces/Command';
 import Agree from './Agree';
 import Ban from './Ban';
+import Faction from './Faction';
 
 class CommandList {
-    private commands: Array<Command<any>> = [new Agree(), new Ban()];
+    private commands: Array<Command<any>> = [
+        new Agree(),
+        new Ban(),
+        new Faction()
+    ];
 
     public getCommands(): Array<Command<any>> {
         return this.commands;
---
+++ src/Commands/Faction.ts
@@ -0,0 +1,142 @@
+import { Message, Role } from 'discord.js';
+import { channels, factions } from '../../config.json';
+import Command from '../Interfaces/Command';
+import FactionParams from '../Types/FactionParams';
+import FactionParamsParser from '../Parsers/FactionParamsParser';
+
+class Faction implements Command<FactionParams> {
+    identifier: string;
+    description: string;
+    numMsg: number;
+    parser: (src: Message, identifier: string) => FactionParams;
+    constructor() {
+        this.identifier = 'Faction';
+        this.description = 'Select a faction to join.';
+        this.parser = FactionParamsParser;
+    }
+
+    public async handle(msg: Message, param: FactionParams): Promise<void> {
+        const { role } = param;
+        console.log(role);
+        this.numMsg = 1;
+
+        // Used to bulk delete messages after the command to keep the channel clean.
+
+        if (msg.channel.id !== channels.enlist) {
+            // do nothing
+            return;
+        }
+
+        if (!msg.member) {
+            return;
+        }
+
+        if (role == undefined) {
+            await this.respond(
+                msg,
+                {
+                    message: 'Please enter a faction to join'
+                },
+                'send'
+            );
+            this.numMsg++;
+        } else if (role == 'remove') {
+            await this.removeRoles(msg);
+            await this.respond(
+                msg,
+                {
+                    message: 'You have been removed from your faction'
+                },
+                'send'
+            );
+            this.numMsg++;
+        } else if (
+            typeof role != 'string' &&
+            msg.member.roles.cache.get(role.id)
+        ) {
+            await this.respond(
+                msg,
+                {
+                    message: 'You already belong to ' + role.name
+                },
+                'send'
+            );
+            this.numMsg++;
+        } else if (
+            typeof role != 'string' &&
+            !msg.member.roles.cache.get(role.id)
+        ) {
+            this.enlistUser(msg, role);
+        } else {
+            await this.respond(
+                msg,
+                {
+                    message:
+                        "Something went wrong and I don't know what. contact @Phinocio."
+                },
+                'send'
+            );
+            this.numMsg++;
+        }
+
+        setTimeout(() => {
+            msg.channel.bulkDelete(this.numMsg);
+        }, 2000);
+    }
+
+    protected async respond(
+        msg: Message,
+        data: object,
+        type: string
+    ): Promise<void> {
+        try {
+            switch (type) {
+                case 'send':
+                    await msg.channel.send(data['message']);
+                    break;
+                case 'reply':
+                    await msg.reply(data['message']);
+                    break;
+            }
+        } catch (e) {
+            console.error(`${this.identifier} response error: ${e.message}`);
+        }
+    }
+
+    protected async removeRoles(msg: Message): Promise<void> {
+        try {
+            for (const faction in factions) {
+                if (
+                    msg.member &&
+                    msg.member.roles.cache.get(factions[faction])
+                ) {
+                    await msg.member.roles.remove(factions[faction]);
+                }
+            }
+        } catch (e) {
+            // TODO: Channel log
+            console.log('Faction remove error:', e.message);
+        }
+    }
+
+    protected async enlistUser(msg: Message, role: Role): Promise<void> {
+        try {
+            // Remove role.
+            await this.removeRoles(msg);
+            await msg.member?.roles.add(role);
+            await this.respond(
+                msg,
+                {
+                    message: 'You have been added to ' + role.name
+                },
+                'send'
+            );
+            this.numMsg++;
+        } catch (e) {
+            // TODO: Channel log
+            console.log('Faction add error:', e.message);
+        }
+    }
+}
+
+export default Faction;
