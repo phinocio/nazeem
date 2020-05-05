f502120ed00042e33d1e0e325f5c7c6aecb35181
Phinocio <github@phinocio.com>
2020-05-05 13:02:15 -0600

    add ask command

---
+++ src/Commands/Ask.ts
@@ -0,0 +1,33 @@
+import { Message } from 'discord.js';
+import Command from '../Interfaces/Command';
+
+class Ask implements Command<undefined> {
+    public identifier: string;
+    public description: string;
+    public usage: string;
+
+    constructor() {
+        this.identifier = 'Ask';
+        this.description = 'Just ~~dance~~ ask!';
+        this.usage = '!ask';
+    }
+
+    public async handle(msg: Message): Promise<void> {
+        this.respond(msg, {
+            message:
+                'Please dont ask if you can ask a question, simply ask the question. Someone will eventually get around to answering you, we have a very helpful community.'
+        });
+
+        // No response.
+    }
+
+    protected async respond(msg: Message, data: object): Promise<void> {
+        try {
+            await msg.channel.send(data['message']);
+        } catch (e) {
+            console.error(`${this.identifier} response error: ${e.message}`);
+        }
+    }
+}
+
+export default Ask;
