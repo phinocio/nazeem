7c7b83d1c9f0559e40786c1a7575f3562502d9a2
Phinocio <github@phinocio.com>
2020-05-05 16:57:11 -0600

    remove commented code

--- src/Commands/Help.ts
+++ src/Commands/Help.ts
@@ -85,52 +85,6 @@ class Help implements Command<HelpParams> {
             console.error(`${this.identifier} response error: ${e.message}`);
         }
     }
-
-    // protected async respond(msg: Message, data: object): Promise<void> {
-    //     const embed = new RichEmbed();
-    //     embed.setAuthor(`${bot.name} Help Menu`, bot.avatar);
-    //     embed.setThumbnail(bot.avatar);
-    //     embed.setColor(0x00ae86);
-
-    //     for (const key in data) {
-    //         embed.addField(data[key].identifier, data[key].description);
-    //     }
-
-    //     embed.setFooter('Type !help {command} for specific command info.');
-
-    //     try {
-    //         await msg.channel.send(embed);
-    //     } catch (e) {
-    //         console.error('Error from help command: ' + e.message);
-    //     }
-    // }
-
-    // private async respondCommand(
-    //     msg: Message,
-    //     data: object,
-    //     command: string
-    // ): Promise<void> {
-    //     if (data) {
-    //         const embed = new RichEmbed();
-    //         embed.setAuthor(`${bot.name} Help Menu`, bot.avatar);
-    //         embed.setThumbnail(bot.avatar);
-    //         embed.setColor(0x00ae86);
-    //         embed.setTitle(
-    //             `${command[0].toUpperCase() + command.slice(1)} help.`
-    //         );
-
-    //         embed.addField('Description', data['description']);
-    //         embed.addField('Usage', data['usage']);
-
-    //         embed.setFooter('Type !help {command} for specific command info.');
-
-    //         try {
-    //             await msg.channel.send(embed);
-    //         } catch (e) {
-    //             console.error('Error from help command: ' + e.message);
-    //         }
-    //     }
-    // }
 }
 
 export default Help;
