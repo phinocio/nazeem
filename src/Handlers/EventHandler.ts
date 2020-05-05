335dfce483ef76e2302ea800ef511540220d26f9
Phinocio <github@phinocio.com>
2020-05-05 17:41:11 -0600

    add twitch notify

--- src/Handlers/EventHandler.ts
+++ src/Handlers/EventHandler.ts
@@ -1,15 +1,18 @@
 import { Client } from 'discord.js';
 import MemberJoin from '../Events/MemberJoin';
+import TwitchNotify from '../Events/TwitchNotify';
 import MessageHandler from '../Handlers/MessageHandler';
 
 class EventHandler {
     private client: Client;
     private memberJoin: MemberJoin;
+    private twitchNotify: TwitchNotify;
     private msgHandler: MessageHandler;
 
     public constructor(client: Client) {
         this.client = client;
         this.memberJoin = new MemberJoin();
+        this.twitchNotify = new TwitchNotify();
         this.msgHandler = new MessageHandler();
     }
 
@@ -29,6 +32,20 @@ class EventHandler {
             // Ensure bot doesn't respond to PM commands/messages.
             if (msg.guild !== null) {
                 this.msgHandler.handle(msg);
+            }
+        });
+
+        this.client.on('presenceUpdate', async (oldPresence, newPresence) => {
+            if (oldPresence?.activities !== newPresence.activities) {
+                newPresence.activities.forEach((activity) => {
+                    switch (activity.type) {
+                        case 'STREAMING':
+                            this.twitchNotify.handle(newPresence);
+                            break;
+                        default:
+                            break;
+                    }
+                });
             }
         });
     }
