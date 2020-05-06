7c215a9b441b59f44c3aa5ec83db003257fde84e
Phinocio <github@phinocio.com>
2020-05-06 15:36:02 -0600

    update storage helper

--- src/Helpers/Storage.ts
+++ src/Helpers/Storage.ts
@@ -2,20 +2,33 @@ import fs from 'fs';
 import path from 'path';
 
 abstract class Storage {
-    // Relative to project root.
-    private static path = 'storage';
+	// Relative to project root.
+	private static path = 'storage';
 
-    public static async read(file: string): Promise<string> {
-        const data = await fs.promises.readFile(path.join(Storage.path, file));
-        return JSON.parse(data.toString());
-    }
+	public static async read(file: string): Promise<Set<unknown>> {
+		let data = new Set();
+		try {
+			const parse = JSON.parse(
+				await fs.promises.readFile(
+					path.join(Storage.path, file),
+					'utf8'
+				)
+			);
+			data = new Set(parse);
+		} catch (e) {
+			// File doesn't exist.
+			console.log(e.message);
+		} finally {
+			return data;
+		}
+	}
 
-    public static async store(file: string, data: object): Promise<void> {
-        await fs.promises.writeFile(
-            path.join(Storage.path, file),
-            JSON.stringify(data)
-        );
-    }
+	public static async store(file: string, data: object): Promise<void> {
+		await fs.promises.writeFile(
+			path.join(Storage.path, file),
+			JSON.stringify(data)
+		);
+	}
 }
 
 export default Storage;
