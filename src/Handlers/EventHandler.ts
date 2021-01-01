8519dbb202524873e2d3a3e7edf8f48366059721
Phinocio <github@phinocio.com>
2021-01-01 13:39:55 -0700

    removed twitch notify

--- src/Handlers/EventHandler.ts
+++ src/Handlers/EventHandler.ts
@@ -1,18 +1,15 @@
 import { Client } from 'discord.js';
 import MemberJoin from '../Events/MemberJoin';
-import TwitchNotify from '../Events/TwitchNotify';
 import MessageHandler from '../Handlers/MessageHandler';
 
 class EventHandler {
 	private client: Client;
 	private memberJoin: MemberJoin;
-	private twitchNotify: TwitchNotify;
 	private msgHandler: MessageHandler;
 
 	public constructor(client: Client) {
 		this.client = client;
 		this.memberJoin = new MemberJoin();
-		this.twitchNotify = new TwitchNotify();
 		this.msgHandler = new MessageHandler();
 	}
 
@@ -32,23 +29,6 @@ class EventHandler {
 			// Ensure bot doesn't respond to PM commands/messages.
 			if (msg.guild !== null) {
 				this.msgHandler.handle(msg);
-			}
-		});
-
-		this.client.on('presenceUpdate', async (oldPresence, newPresence) => {
-			// Check if the oldPresence had a streaming activity.
-			const oldStream = oldPresence?.activities.some(
-				(activity) => activity.type === 'STREAMING'
-			);
-
-			// Check if the oldPresence has a streaming activity.
-			const newStream = newPresence?.activities.some(
-				(activity) => activity.type === 'STREAMING'
-			);
-
-			// Only show notification if old presence didn't have a streaming activity and new one does.
-			if (!oldStream && newStream) {
-				this.twitchNotify.handle(newPresence);
 			}
 		});
 	}
