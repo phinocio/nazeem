231f79f0b4df28b4b8c0d2c56571810ed43c0976
Phinocio <github@phinocio.com>
2021-08-22 13:51:29 -0600

    update and remove unused commands

--- src/Commands/CommandList.ts
+++ src/Commands/CommandList.ts
@@ -1,42 +1,42 @@
 import Command from '../Interfaces/Command';
-import Agree from './Agree';
-import Ask from './Ask';
-import Ban from './Ban';
-import Clear from './Clear';
+// import Agree from './Agree';
+// import Ask from './Ask';
+// import Ban from './Ban';
+// import Clear from './Clear';
 import Faction from './Faction';
-import FAQ from './FAQ';
-import GameFolderFiles from './GameFolderFiles';
+// import FAQ from './FAQ';
+// import GameFolderFiles from './GameFolderFiles';
 import Help from './Help';
-import Kick from './Kick';
+// import Kick from './Kick';
 import Memsize from './Memsize';
-import Mute from './Mute';
+// import Mute from './Mute';
 import NotifyMe from './NotifyMe';
-import Pins from './Pins';
+// import Pins from './Pins';
 import SlowMode from './SlowMode';
-import StabilityTest from './StabilityTest';
-import Unban from './Unban';
-import Unmute from './Unmute';
+// import StabilityTest from './StabilityTest';
+// import Unban from './Unban';
+// import Unmute from './Unmute';
 import UpdatePatreonApi from './UpdatePatreonApi';
 
 class CommandList {
 	private commands: Array<Command<any>> = [
-		new Agree(),
-		new Ask(),
-		new Ban(),
-		new Clear(),
+		// new Agree(),
+		// new Ask(),
+		// new Ban(),
+		// new Clear(),
 		new Faction(),
-		new FAQ(),
-		new GameFolderFiles(),
+		// new FAQ(),
+		// new GameFolderFiles(),
 		new Help(),
-		new Kick(),
+		// new Kick(),
 		new Memsize(),
-		new Mute(),
+		// new Mute(),
 		new NotifyMe(),
-		new Pins(),
+		// new Pins(),
 		new SlowMode(),
-		new StabilityTest(),
-		new Unban(),
-		new Unmute(),
+		// new StabilityTest(),
+		// new Unban(),
+		// new Unmute(),
 		new UpdatePatreonApi()
 	];
 
